a882c0ba20e4399067df9a61e9e9f5e553bf6169
Vladislav Cherkasov <vlad0s74chel@gmail.com>
2026-06-23 16:47:20 +0500

    fix await password

--- flutter/lib/custom/password_gen.dart
+++ flutter/lib/custom/password_gen.dart
@@ -3,10 +3,10 @@ import 'package:flutter_hbb/models/platform_model.dart';
 /// Current temporary password for activation API / backend sync.
 /// Refreshes once if the server has not generated a password yet.
 Future<String> getTemporaryPassword() async {
-  var password = bind.mainGetTemporaryPassword();
+  var password = await bind.mainGetTemporaryPassword();
   if (password.isEmpty) {
-    bind.mainUpdateTemporaryPassword();
-    password = bind.mainGetTemporaryPassword();
+    await bind.mainUpdateTemporaryPassword();
+    password = await bind.mainGetTemporaryPassword();
   }
   if (password.isEmpty) {
     throw StateError('Temporary password is not available');
